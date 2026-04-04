456b89e2097e574d9c1b0f85aae6bf34909fc331
ratul43 <kmratul971@gmail.com>
2026-04-05 00:44:31 +0600

    admin delete a user

--- index.js
+++ index.js
@@ -80,6 +80,13 @@ async function run() {
       res.send(result);
     });
 
+    // admin delete a user
+    app.delete("/users", async (req, res) => {
+      const email = req.query.email;
+      const result = await usersCollection.deleteOne({ email: email });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
